c98f97200b7c3cbd759f58463595dd9a75b94c2b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-10-27 15:32:32 -0300

    Use gin as router

--- decks/main.go
+++ decks/main.go
@@ -1,10 +1,23 @@
 package main
 
+import "log"
+
+import "github.com/gin-gonic/gin"
+
 
 func main() {
+    router := gin.Default()
 	address, node := NodeFromCLI()
 
 	node.StartLeaderLoop()
-	node.AddRoutes()
-	node.Serve(address)
+	node.AddRoutes(router)
+
+    log.Printf("Node%d@%s: Leader%d@%s, peers=[%v]", 
+        node.id,
+        address,
+        node.leaderID,
+        node.leaderAddr,
+        node.peers,
+    )
+    router.Run(address)
 }
--- decks/server.go
+++ decks/server.go
@@ -1,38 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-)
+import "github.com/gin-gonic/gin"
 
-func (node *Node) Serve(listenAddr Address) {
-	log.Printf("node %d starting on %s, peers=%v, leader=%d (%s)", node.id, listenAddr, node.peers, node.leaderID, node.leaderAddr)
-	if err := http.ListenAndServe(listenAddr, nil); err != nil {
-		fmt.Fprintln(os.Stderr, "server error:", err)
-		os.Exit(1)
-	}
-}
 
-func (node *Node) AddRoutes() {
-	http.HandleFunc("/cards", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			node.handleGetCards(w, r)
-		case "POST":
-			node.handlePostCard(w, r)
-		default:
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		}
-	})
-	http.HandleFunc("/cards/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "DELETE" {
-			node.handleDeleteCard(w, r)
-			return
-		}
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-	})
-	http.HandleFunc("/replicate", node.handleReplicate)
-	http.HandleFunc("/status", node.handleStatus)
+/// Add Routes to a Node
+func (node *Node) AddRoutes(router *gin.Engine) {
+	// Domain endpoints
+	router.GET("/cards", gin.WrapF(node.handleGetCards))
+	router.POST("/cards", gin.WrapF(node.handlePostCard))
+	router.DELETE("/cards/:id", gin.WrapF(node.handleDeleteCard))
+
+	// Peer endpoints
+	router.GET("/status", gin.WrapF(node.handleStatus))
+
+	// Private endpoints
+	router.POST("/replicate", gin.WrapF(node.handleReplicate))
 }
