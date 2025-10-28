9264dbdb6e9196ab99e7f3785668f51e418aa5ac
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-10-28 14:40:35 -0300

    Implement frontend

--- decks/server.go
+++ decks/server.go
@@ -4,6 +4,22 @@ import "github.com/gin-gonic/gin"
 
 // / Add Routes to a Node
 func (node *Node) AddRoutes(router *gin.Engine) {
+	// -- Frontend pages --
+
+	router.Static("/decks/static", "./decks/frontend")
+	router.GET("/", func(c *gin.Context) {
+		c.File("./decks/frontend/main.html")
+	})
+	router.GET("/admin", func(c *gin.Context) {
+		c.File("./decks/frontend/admin.html")
+	})
+	router.GET("/user", func(c *gin.Context) {
+		c.File("./decks/frontend/user.html")
+	})
+	router.GET("/trade", func(c *gin.Context) {
+		c.File("./decks/frontend/trade.html")
+	})
+
 	// -- User endpoints --
 	router.GET("/users/:user/claim", gin.WrapF(node.handleClaim))
 	router.GET("/users/:user/cards", gin.WrapF(node.handleGetCards))
