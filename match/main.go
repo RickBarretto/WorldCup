cb6a7a7edaf381e2b1f45806e15ac93c57b6ca61
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-10-28 13:45:08 -0300

    Move function to main

--- match/main.go
+++ match/main.go
@@ -1,11 +1,34 @@
 package main
 
-
-
-
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	cli := parseCli()
 	StartServer(cli.address, cli.peers)
 }
 
+func StartServer(address Address, peers []Address) {
+	server := NewServer(address)
+	for _, p := range peers {
+		if p != "" {
+			server.AddPeer(p)
+		}
+	}
+
+	// -- API --
+	http.HandleFunc("/ws", server.upgradeWebsocket())
+	http.HandleFunc("/play", server.playMatch())
+	http.HandleFunc("/find-waiter", server.FindWaiter)
+	http.HandleFunc("/start-remote-match", server.startRemoteMatch())
+	http.HandleFunc("/peers", server.managePeers())
+
+	// -- Frontend --
+	fs := http.FileServer(http.Dir("./match/frontend"))
+	http.Handle("/", fs)
+
+	log.Printf("match server listening on %s\n", address)
+	log.Fatal(http.ListenAndServe(address, nil))
+}
--- match/ws.go
+++ match/ws.go
@@ -1,7 +1,5 @@
 package main
 
-import "log"
-import "net/http"
 import "sync"
 import "time"
 
@@ -29,25 +27,3 @@ func (player *PlayerConnection) sendJSON(value any) {
 	_ = player.connection.WriteJSON(value)
 }
 
-func StartServer(address Address, peers []Address) {
-	server := NewServer(address)
-	for _, p := range peers {
-		if p != "" {
-			server.AddPeer(p)
-		}
-	}
-
-	// -- API --
-	http.HandleFunc("/ws", server.upgradeWebsocket())
-	http.HandleFunc("/play", server.playMatch())
-	http.HandleFunc("/find-waiter", server.FindWaiter)
-	http.HandleFunc("/start-remote-match", server.startRemoteMatch())
-	http.HandleFunc("/peers", server.managePeers())
-
-	// -- Frontend --
-	fs := http.FileServer(http.Dir("./match/frontend"))
-	http.Handle("/", fs)
-
-	log.Printf("match server listening on %s\n", address)
-	log.Fatal(http.ListenAndServe(address, nil))
-}
